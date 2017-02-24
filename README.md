122cf2e234971fae9fa22850a4c5096fca22d297
Mimieam <t.echismimi@gmail.com>
2017-02-24 08:59:16 -0600

    Antd Added

---
+++ README.md
@@ -0,0 +1,34 @@
+## Goal of this project
+Creating a daily task logger app in React
+
+## Component / Container
+
+**UserInput**
+  - [ ] has a Input textbox and a data picker
+  - [ ] on submit ( of a button ) will save it's content to redux store
+
+**logView / TimelineCard**
+  - [ ] an Card displaying the each task performed by the user this current week.
+  - [ ] renders timeline items
+
+**task**
+  - [ ] a timeline items with a date
+  - [ ]can be edited 
+    *  [ ]ondblClick create a modal form 
+  - [ ]as tag with an icon indicating it's status
+    * [ ]started
+    * [ ]in progress
+    * [ ]completed
+    * [ ]abandoned
+
+**What else do we need for component?**
+> ...
+
+**Dev Stack**
+  *React
+  *antd
+  *redux
+  *hmmm just forgot what i was about to write... oh well
+  *Less... that's what i forgot -_-
+  *webpack ?? maybe
+
---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+import Button from 'antd/lib/button';
+import 'antd/lib/button/style/css';
+
+import Timeline from 'antd/lib/timeline';
+import 'antd/lib/timeline/style/css'; 
+
+import Card from 'antd/lib/card';
+import 'antd/lib/card/style/css';
+
+import DatePicker from 'antd/lib/date-picker';
+import 'antd/lib/date-picker/style/css';
+
+import Input from 'antd/lib/input';
+import 'antd/lib/input/style/css';
+const InputGroup = Input.Group;
+
+import Icon from 'antd/lib/icon';
+import 'antd/lib/icon/style/css';
+
+import { Rate } from 'antd'
+import 'antd/lib/rate/style/css';
+
+import { Tag } from 'antd'
+import 'antd/lib/tag/style/css';
+
+import enUS from 'antd/lib/date-picker/locale/en_US';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>TimeLog</h2>
+        </div>
+
+        <InputGroup compact>
+          <Input style={{ width: '50%' }} defaultValue="input content" />
+          <DatePicker
+            defaultValue={moment()}
+            format={'MM/DD/YY - h:mm:ss a'}
+            locale={enUS}
+            showTime
+          />
+        </InputGroup>
+        <hr />
+
+        <Card className="App-intro" title="Week 1 " extra={<a href="#">More</a>} style={{ width: 400, textAlign: 'initial' }}>
+          <Timeline>
+            <Timeline.Item>Create a services site 2015-09-01 <Rate disabled defaultValue={2} /></Timeline.Item>
+            <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+            <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">Technical testing 2015-09-01</Timeline.Item>
+            <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+            <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+          </Timeline>
+        </Card>
+        <Card className="App-intro" title="Week 1 " extra={<a href="#">More</a>} style={{ width: 400, textAlign: 'initial' }}>
+          <Timeline>
+            <Timeline.Item><Tag color="red">red</Tag>Create a services site 2015-09-01 </Timeline.Item>
+            <Timeline.Item>Solve  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, modi nesciunt explicabo deleniti maxime ea deserunt dolor hic molestiae tempore nobis quibusdam beatae, sit laudantium expedita architecto! Quae, sit, vitae. iniloremtial network problems 2015-09-01</Timeline.Item>
+            <Timeline.Item dot={<Icon type="cloud-o" style={{ fontSize: '16px' }} />} color="red">Technical testing 2015-09-01</Timeline.Item>
+            <Timeline.Item >Technical testing 2015-09-01</Timeline.Item>
+            <Timeline.Item>Network iniloremtial
+              iniloremtial  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, quasi commodi dolorum natus aliquid architecto consectetur officia, minus laudantium doloremque repellendus nihil possimus, temporibus voluptates at, saepe nostrum tempora ea! ipsum dolor sit amet, consectetur adipisicing elit. Officia vel atque ipsam labore enim accusamus aliquam eligendi, repudiandae necessitatibus totam. Inventore et cupiditate aliquam laudantium, praesentium dolorem ipsa rem, deserunt. ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, nesciunt provident ducimus harum autem temporibus? Facere veniam, odio expedita suscipit iste, autem modi cumque id nam dignissimos nostrum corrupti! Repellendus. ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam debitis, quod molestias earum qui soluta ipsam accusantium aspernatur id rerum blanditiis nostrum repellat, perspiciatis deserunt error. Delectus, sint esse aut? problems being solved 2015-09-01<Rate defaultValue={5} /></Timeline.Item>
+          </Timeline>
+        </Card>
+        <Button>Yo</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
